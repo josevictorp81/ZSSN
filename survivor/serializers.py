23bd9227ba042299392e32b80178f25715b6415f
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2023-04-06 16:24:44 -0300

    create serializers of Resource and Survivor

---
+++ survivor/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from core.models import Resource, Survivor
+
+class ResourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Resource
+        fields = ['id', 'name', 'quantity', 'survivor']
+        read_only_fields = ['id']
+
+
+class SurvivorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Survivor
+        fields = ['id', 'name', 'age', 'sex', 'last_local']
+        read_only_fields = ['id']
