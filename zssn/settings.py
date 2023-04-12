f4e5fe97a03961d07aa84719ccef741e46adf475
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2023-04-12 13:40:26 -0300

    settings

--- zssn/settings.py
+++ zssn/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--61dty0e-3==l3i)wk@gdoy6*mke6@rmnqavoz^-wirb^0c1hv'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
